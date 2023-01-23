fdf8c742679ae00d2a509818142f5bee95893b8b
cathalduffy <cathalduffy89@gmail.com>
2023-01-23 20:39:51 +0000

    Add dashboard page e2e tests

---
+++ cypress/e2e/tests/dashboard.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+import {slowCypressDown} from 'cypress-slow-down'
+
+const email = 'cathal@test.com'
+const password = '123456'
+
+
+describe('Dashboard page tests', () => {
+    beforeEach(() => {
+      cy.visit('https://football-app-login.web.app/login')
+      cy.get('#root > div > div > div.card-body > div:nth-child(1) > input').type(`${email}`)
+      cy.get('#root > div > div > div.card-body > div:nth-child(2) > input').type(`${password}`)
+      cy.xpath(`//button[text()='Login']`).click()
+    })
+    it('Verify that team cards are visible to user', () => {  
+        cy.xpath(`//*[text()='Arsenal']`).should('have.text', 'Arsenal')
+ 
+    })
+
+    // it('Verify that team images are visible to user', () => {   
+    //     cy.get('#root > div > div > div.card-body > div:nth-child(1) > input').type(`${email}`)
+    // })
+    // it('Verify functionality of League dropdown menu', () => {   
+    //     cy.get('#root > div > div > div.card-body > div:nth-child(1) > input').type(`${email}`)
+    // })
+    // it('Verify functionality of Year dropdown menu', () => {   
+    //     cy.get('#root > div > div > div.card-body > div:nth-child(1) > input').type(`${email}`)
+    // })
+
+    afterEach(() => {
+      cy.clearCookies()
+    })
+
+  })
--- cypress/e2e/tests/login.cy.js
+++ cypress/e2e/tests/login.cy.js
@@ -1,14 +1,17 @@
 /// <reference types="cypress" />
 import {slowCypressDown} from 'cypress-slow-down'
 
+const email = 'cathal@test.com'
+const password = '123456'
+
 
 describe('Login football app', () => {
     beforeEach(() => {
       cy.visit('https://football-app-login.web.app/login')
     })
     it('Successfully log in with valid username and password', () => {   
-      cy.get('#root > div > div > div.card-body > div:nth-child(1) > input').type('cathal@test.com')
-      cy.get('#root > div > div > div.card-body > div:nth-child(2) > input').type('123456')
+      cy.get('#root > div > div > div.card-body > div:nth-child(1) > input').type(`${email}`)
+      cy.get('#root > div > div > div.card-body > div:nth-child(2) > input').type(`${password}`)
       cy.xpath(`//button[text()='Login']`).click()
       cy.xpath(`//h3[text()='Teams Dashboard']`)
       cy.xpath(`//a[text()='Logout']`).click()
@@ -16,7 +19,7 @@ describe('Login football app', () => {
     })
 
       it('Verify login unsuccessful when valid username and invalid password', () => {   
-        cy.get('#root > div > div > div.card-body > div:nth-child(1) > input').type('cathal@test.com')
+        cy.get('#root > div > div > div.card-body > div:nth-child(1) > input').type(`${email}`)
         cy.get('#root > div > div > div.card-body > div:nth-child(2) > input').type('fakepassword')
         cy.xpath(`//button[text()='Login']`).click()
       })
